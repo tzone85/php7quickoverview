59ed33cc2aff8bd4f2b90c3625d4116ef7fe7d9e
thando mini <tzone85@gmail.com>
2019-02-08 00:25:01 +0200

    Used spaceship operator by sorting user objects

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // spaceship operator
 
-$games = ['Mass Effect', 'Super Mario Bros', 'Zeda', 'Follout', 'Metal Gear'];
+/*$games = ['Mass Effect', 'Super Mario Bros', 'Zeda', 'Follout', 'Metal Gear'];
 
 //rsort($games);
 
@@ -10,3 +10,64 @@ usort($games, function ($a, $b){
 	return strlen($a) <=> strlen($b); // return -1, 0, 1
 });
 var_dump($games);
+*/
+
+class User {
+	protected $name;
+	protected $age;
+
+	public function __construct($name, $age)
+	{
+		$this->name = $name;
+		$this->age = $age;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getUsers()
+	{
+		return $this->name;
+	}
+
+	public function getAge()
+	{
+		return $this->age;
+	}
+}
+
+class UserCollection {
+	protected $users;
+
+	public function __construct(array $users)
+	{
+		$this->users = $users;
+	}
+
+	public function sortBy($property)
+	{
+		usort($this->users, function($userOne, $userTwo) use ($property){
+			return $userOne->$property() <=> $userTwo->$property();
+		});
+
+//		return $this->users;
+	}
+
+	public function users()
+	{
+		return $this->users;
+	}
+}
+
+$collection = new UserCollection([
+	new User('Jeff', 30),
+	new User('Taylor', 29),
+	new User('Jane', 50),
+	new User('Susia', 10),
+]);
+
+$collection->sortBy('getName');
+
+var_dump($collection->users());
